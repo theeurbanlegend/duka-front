892b7eb77f78a1589bb748b13f16967c5142f65f
Bewton <mbuto152@gmail.com>
2024-03-25 19:47:09 +0300

    Added barcode url

--- src/components/forms/AddDrug.jsx
+++ src/components/forms/AddDrug.jsx
@@ -135,7 +135,7 @@ const AddDrug = () => {
                         <label htmlFor="barcode" className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Barcode</label>
                         <input type="text" id="barcode" readOnly value={barcode} onChange={(e) => setBarcode(e.target.value)} className="shadow-sm rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
                     </div>
-                        <a href="#"
+                        <a href="https://barcode-fcn6.onrender.com"
                             className="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan from here</a>
                     </div>
                     }
